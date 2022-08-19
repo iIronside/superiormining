59cf0ba3135be878084683c8f052e05e1a95ba04
mrgoodcat <fmarkofff@yandex.com>
2022-08-19 16:26:10 +0300

    update css

--- frontend/views/layouts/broker.php
+++ frontend/views/layouts/broker.php
@@ -14,6 +14,7 @@
     <link href="/build/styles/brokerPage.css" rel="stylesheet">
     <link href="/build/styles/brokerRating.css" rel="stylesheet">
     <link href="/build/styles/reviewItem.css" rel="stylesheet">
+    <link href="/build/styles/tariffItem.css" rel="stylesheet">
     <?php $this->head() ?>
 </head>
 <body>
